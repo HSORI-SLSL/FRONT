2a8adfb601099525d981d02a1a9eacfe511f8e04
jOyEjiN2 <angel000604@naver.com>
2023-06-01 16:56:49 +0900

    6/1 스크롤바2

--- src/chatroom.js
+++ src/chatroom.js
@@ -1,24 +1,24 @@
 import React, {useState} from 'react';
 import './chatroom.css';
 
-
 function Chatroom() {
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-
 
   const handleMessageChange = (event) => {
     setMessage(event.target.value);
   };
 
   const handleMessageSubmit = () => {
-    // 서버에 메시지 전송 로직 추가
-    console.log('메시지 전송:', message);
-
-    setMessages([...messages, message]);
-
-    // 메시지 입력 필드 초기화
-    setMessage('');
+    if (message.trim() !== '') {
+      // 서버에 메시지 전송 로직 추가
+      console.log('메시지 전송:', message);
+  
+      setMessages([...messages, message]);
+  
+      // 메시지 입력 필드 초기화
+      setMessage('');
+    }
   };
 
   const enterKeyEventHandler = (e) => {
@@ -28,39 +28,29 @@ function Chatroom() {
   }
 
   return (
-
     <div className="chat-room" class="container">
-
-
-     
-      {/* 대화 상대 */}
-      <div class="chatheader" className="chat-header-card" style={{ position: 'fixed', top: '20px', left: 270, width: 'calc(96% - 250px)', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      {/* 대화 상대 나타내는 부분*/}
+      <div class="chatheader" className="chat-header-card">
         <div class="card-body d-flex justify-content-center align-items-center">
           세종대왕
         </div>
       </div>
 
-
-
-      {/* 주고받는 메시지 */}
-      <div id='chatbox' className="chat-messages" style={{ height: '500px', marginTop: '80px', padding: '10px', overflowY: 'scroll', overflowX : 'hidden', maxHeight: '100vh'}}>
+      {/* 메시지 주고받는 부분*/}
+      <div className="chat-messages">
         {messages.map((msg, index) => (
-          <div className="message" style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end'}} key={index}>
-            <div className="text"  style={{ wordWrap: 'break-word', maxWidth: '50%', padding: '10px', borderRadius: '5px', backgroundColor: '#f2f2f2', display: 'inline-block', whiteSpace: 'pre-wrap', overflowWrap: 'break-word', wordWrap: 'break-word'}}>{msg}</div>
+          <div className="message" key={index}>
+            <div className="text" >{msg}</div>
           </div>
         ))}
       </div>
 
-   
-
-    
-
-      {/* 메시지 입력 */}
-      <div class="chatfooter" className="chat-input" style={{ position: 'fixed', bottom: 0, left: 270, marginBottom:10, width: 'calc(96% - 250px)'}}>
+      {/* 메시지 입력 부분*/}
+      <div class="chatfooter" className="chat-input">
         <div class="input-group input-group-lg">
           {/* 퀴즈 버튼 */}
           <button type="button" class="btn btn-chat">  Q </button>
-          {/* 메시지 입력 부분 */}
+          {/* 메시지 입력 */}
           <input 
           type="text" 
           class="form-control" 
@@ -80,9 +70,7 @@ function Chatroom() {
           </button>
         </div>
       </div>
-
-      </div>
-
+    </div>
   );
 }
 
