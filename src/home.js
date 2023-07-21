8f1f89ed6189bd4e7ed504d671fd8d046c037a15
seul-young <tmfud@naver.com>
2023-07-21 14:46:35 +0900

    인물정보 완료

--- src/home.js
+++ src/home.js
@@ -20,6 +20,7 @@ function Home() {
             <div className="person-info-text">
               <h6>세종대왕</h6>
               <p>1397.05.15 ~ 1450.03.30</p>
+              <p>제 4대</p>
             </div>
           </div>
           <div className="person-info-item">
@@ -29,6 +30,7 @@ function Home() {
             <div className="person-info-text">
               <h6>태종</h6>
               <p>1367.06.13 ~ 1422.05.30</p>
+              <p>제 3대</p>
             </div>
           </div>
           <div className="person-info-item">
@@ -38,6 +40,7 @@ function Home() {
             <div className="person-info-text">
               <h6>영조</h6>
               <p>1694.10.31 ~ 1776.04.22</p>
+              <p>제 21대</p>
             </div>
           </div>
         </div>
@@ -60,12 +63,14 @@ function Home() {
         </div>
 
         {/* 추천 콘텐츠 */}
+        <Link to="/contents" className="link-black">
         <div className="recommended-content">
           <div className="content-header">
           <h5>추천콘텐츠</h5>
           </div>
           <img src="/img/abc.png" alt="Bot Avatar" className="avatar" width="250px" height="300px" />
         </div>
+        </Link>
         
       </div>
     </div>
