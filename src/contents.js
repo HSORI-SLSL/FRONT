84997f85a2c8cb0b63bd273ebab63f24a26b53d1
jOyEjiN2 <angel000604@naver.com>
2023-08-02 17:20:30 +0900

    연관콘텐츠 기능 구현 완료

--- src/contents.js
+++ src/contents.js
@@ -1,105 +1,107 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import './contents.css';
 
 const ContentList = () => {
-  const [contents, setContents] = useState([]);
+  const [watchaContents, setWatchaContents] = useState([]);
+  const [youtubeContents, setYoutubeContents] = useState([]);
+  const [selectedKing, setSelectedKing] = useState(''); // Initialize with empty string
 
   useEffect(() => {
-    // 백엔드 API 호출을 위한 함수 정의
-    const fetchContentsFromBackend = async () => {
-      try {
-        const response = await axios.post(
-          'https://0218-1-231-206-74.ngrok-free.app/query/CRAWL',
-          {
-            query: '세종대왕',
-          },
-          {
-            withCredentials: false,
-          }
-        );
-        setContents(response.data.contents); // Use 'contents' array from the API response
-      } catch (error) {
-        console.error('Error fetching contents from backend:', error);
+    const fetchData = async () => {
+      if (selectedKing) {
+        try {
+          const responseWatcha = await axios.post(
+            'https://0218-1-231-206-74.ngrok-free.app/query/CRAWL',
+            {
+              query: selectedKing,
+            },
+            {
+              withCredentials: false,
+            }
+          );
+
+          const responseYoutube = await axios.post(
+            'https://0218-1-231-206-74.ngrok-free.app/query/CRAWLY',
+            {
+              query: selectedKing,
+            },
+            {
+              withCredentials: false,
+            }
+          );
+
+          setWatchaContents(responseWatcha.data.contents);
+          setYoutubeContents(responseYoutube.data.contents);
+        } catch (error) {
+          console.error('Error fetching contents from backend:', error);
+        }
+      } else {
+        // Clear contents if no king is selected
+        setWatchaContents([]);
+        setYoutubeContents([]);
       }
     };
 
-    fetchContentsFromBackend(); // 함수 호출로 API 데이터 가져오기
-  }, []);
-
-
-   // 카테고리별로 콘텐츠를 분류하는 함수
-   const groupContentsByCategory = () => {
-    const groupedContents = {
-      '영화': [],
-      'TV 프로그램': [],
-      '책': []
-    };
-
-    contents.forEach((content) => {
-      const { category } = content;
-      if (groupedContents[category]) {
-        groupedContents[category].push(content);
-      }
-    });
-
-    return groupedContents;
-  };
-
-  const groupedContents = groupContentsByCategory();
-
-
-
+    fetchData();
+  }, [selectedKing]);
 
   return (
-    <div>
-      <h2>콘텐츠 목록</h2>
-      <ul style={{ display: 'flex' }}>
-        {/* '영화' 카테고리 */}
-        <li>
-          <h3 style={{ marginTop: '50px', marginBottom: '25px'}}>영화</h3>
-          <ul style={{ display: 'flex', listStyleType: 'none' }}>
-            {groupedContents['영화'].map((content, index) => (
-              <li key={index}>
-                <a href={content.href} target="_blank" rel="noopener noreferrer">
-                  <img src={content.img_urls} alt={content.title} width='150px' style={{ marginRight: '30px' }} />
-                </a>
-                <h5 style={{ marginTop: '10px'}}>{content.title}</h5>
+    <div className='content-list'>
+      <div className='king-buttons'>
+        <button onClick={() => setSelectedKing('세종대왕')}>세종대왕</button>
+        <button onClick={() => setSelectedKing('태종')}>태종</button>
+        <button onClick={() => setSelectedKing('영조')}>영조</button>
+      </div>
+      <div className='selected-king'>
+        {selectedKing ? (
+          <h2>{selectedKing}의 콘텐츠</h2>
+        ) : (
+          <h2>왕을 선택하세요</h2>
+        )}
+      </div>
+      <div className='watcha'>
+        <h3>왓챠 콘텐츠</h3>
+        <ul className='horizontal-list'>
+          {watchaContents.map((content, index) => (
+            <li key={index} className='horizontal-list-item'>
+              <a href={content.href} target='_blank' rel='noopener noreferrer'>
+                <img
+                  src={content.img_urls}
+                  alt={content.title}
+                  width='150px'
+                  style={{ marginRight: '10px' }}
+                />
+              </a>
+              <div style={{ display: 'inline-block' }}>
+                <h3>{content.title}</h3>
                 <small>{content.info}</small>
-              </li>
-            ))}
-          </ul>
-        </li>
-        {/* 'TV 프로그램' 카테고리 */}
-        <li>
-          <h3 style={{ marginTop: '30px', marginBottom: '25px'}}>TV 프로그램</h3>
-          <ul style={{ display: 'flex', listStyleType: 'none' }}>
-            {groupedContents['TV 프로그램'].map((content, index) => (
-              <li key={index}>
-                <a href={content.href} target="_blank" rel="noopener noreferrer">
-                  <img src={content.img_urls} alt={content.title} width='150px' style={{ marginRight: '30px' }} />
-                </a>
-                <h5 style={{ marginTop: '10px'}}>{content.title}</h5>
-                <small>{content.info}</small>
-              </li>
-            ))}
-          </ul>
-        </li>
-        {/* '책' 카테고리 */}
-        <li>
-          <h3 style={{ marginTop: '30px', marginBottom: '25px'}}>책</h3>
-          <ul style={{ display: 'flex', listStyleType: 'none' }}>
-            {groupedContents['책'].map((content, index) => (
-              <li key={index}>
-                <a href={content.href} target="_blank" rel="noopener noreferrer">
-                  <img src={content.img_urls} alt={content.title} width='150px' style={{ marginRight: '30px' }} />
-                </a>
-                <h5 style={{ marginTop: '10px'}}>{content.title}</h5>
-                <small>{content.info}</small>
-              </li>
-            ))}
-          </ul>
-        </li>
-      </ul>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className='youtube'>
+        <h3>유튜브 콘텐츠</h3>
+        <ul className='horizontal-list'>
+          {youtubeContents.map((content, index) => (
+            <li key={index} className='horizontal-list-item'>
+              <a href={content.url} target='_blank' rel='noopener noreferrer'>
+                <img
+                  src={content.thumbnail}
+                  alt={content.title}
+                  width='150px'
+                  style={{ marginRight: '10px' }}
+                />
+              </a>
+              <div style={{ display: 'inline-block' }}>
+                <h3>{content.title}</h3>
+                <p>{content.channelName}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
