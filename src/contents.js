5e9847a3fad5168f491fc90845fdbc51bc9e17a3
seul-young <tmfud@naver.com>
2023-08-02 16:07:01 +0900

    연관콘텐츠 배치

--- src/contents.js
+++ src/contents.js
@@ -9,7 +9,7 @@ const ContentList = () => {
     const fetchContentsFromBackend = async () => {
       try {
         const response = await axios.post(
-          'https://52e3-1-231-206-74.ngrok-free.app/query/CRAWL',
+          'https://0218-1-231-206-74.ngrok-free.app/query/CRAWL',
           {
             query: '세종대왕',
           },
@@ -26,20 +26,79 @@ const ContentList = () => {
     fetchContentsFromBackend(); // 함수 호출로 API 데이터 가져오기
   }, []);
 
+
+   // 카테고리별로 콘텐츠를 분류하는 함수
+   const groupContentsByCategory = () => {
+    const groupedContents = {
+      '영화': [],
+      'TV 프로그램': [],
+      '책': []
+    };
+
+    contents.forEach((content) => {
+      const { category } = content;
+      if (groupedContents[category]) {
+        groupedContents[category].push(content);
+      }
+    });
+
+    return groupedContents;
+  };
+
+  const groupedContents = groupContentsByCategory();
+
+
+
+
   return (
     <div>
       <h2>콘텐츠 목록</h2>
-      <ul>
-        {contents.map((content, index) => (
-          <li key={index}>
-            <a href={content.href} target="_blank" rel="noopener noreferrer">
-              <img src={content.img_urls} alt={content.title} />
-            </a>
-            <h3>{content.title}</h3>
-            <p>{content.info}</p>
-            <p>카테고리: {content.category}</p>
-          </li>
-        ))}
+      <ul style={{ display: 'flex' }}>
+        {/* '영화' 카테고리 */}
+        <li>
+          <h3 style={{ marginTop: '50px', marginBottom: '25px'}}>영화</h3>
+          <ul style={{ display: 'flex', listStyleType: 'none' }}>
+            {groupedContents['영화'].map((content, index) => (
+              <li key={index}>
+                <a href={content.href} target="_blank" rel="noopener noreferrer">
+                  <img src={content.img_urls} alt={content.title} width='150px' style={{ marginRight: '30px' }} />
+                </a>
+                <h5 style={{ marginTop: '10px'}}>{content.title}</h5>
+                <small>{content.info}</small>
+              </li>
+            ))}
+          </ul>
+        </li>
+        {/* 'TV 프로그램' 카테고리 */}
+        <li>
+          <h3 style={{ marginTop: '30px', marginBottom: '25px'}}>TV 프로그램</h3>
+          <ul style={{ display: 'flex', listStyleType: 'none' }}>
+            {groupedContents['TV 프로그램'].map((content, index) => (
+              <li key={index}>
+                <a href={content.href} target="_blank" rel="noopener noreferrer">
+                  <img src={content.img_urls} alt={content.title} width='150px' style={{ marginRight: '30px' }} />
+                </a>
+                <h5 style={{ marginTop: '10px'}}>{content.title}</h5>
+                <small>{content.info}</small>
+              </li>
+            ))}
+          </ul>
+        </li>
+        {/* '책' 카테고리 */}
+        <li>
+          <h3 style={{ marginTop: '30px', marginBottom: '25px'}}>책</h3>
+          <ul style={{ display: 'flex', listStyleType: 'none' }}>
+            {groupedContents['책'].map((content, index) => (
+              <li key={index}>
+                <a href={content.href} target="_blank" rel="noopener noreferrer">
+                  <img src={content.img_urls} alt={content.title} width='150px' style={{ marginRight: '30px' }} />
+                </a>
+                <h5 style={{ marginTop: '10px'}}>{content.title}</h5>
+                <small>{content.info}</small>
+              </li>
+            ))}
+          </ul>
+        </li>
       </ul>
     </div>
   );
