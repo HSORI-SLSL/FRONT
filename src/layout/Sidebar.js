60995aa61d50bcf11753e64954a213112588763b
jOyEjiN2 <angel000604@naver.com>
2023-11-03 17:34:08 +0900

    사용자 아이콘 변경

--- src/layout/Sidebar.js
+++ src/layout/Sidebar.js
@@ -75,11 +75,12 @@ function Sidebar() {
             </a>
           </div>
           
-          <svg xmlns="http://www.w3.org/2000/svg" width="80" height="30" fill="currentColor" className="bi bi-person" viewBox="0 0 16 16" style={{ marginTop: '10px', marginLeft: '85px' }}>
+          {/* <svg xmlns="http://www.w3.org/2000/svg" width="80" height="30" fill="currentColor" className="bi bi-person" viewBox="0 0 16 16" style={{ marginTop: '10px', marginLeft: '85px' }}>
             <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
-          </svg>
+          </svg> */}
+          <img src="/img/sayongja2.png" className="sayongja"></img>
           
-          <div className='user'> {userEmail ? `${userEmail}` : '사용자'} </div>
+          <div className='user'> {userEmail ? `${userEmail}` : '실록샐록'} </div>
 
           <span className="fs-5 fw-semibold" style={{ marginTop: '90px' }}>
             대화 목록
