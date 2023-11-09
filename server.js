53f92e97124c9c64ce0631aca5b2b62eca969d2d
seullll <89897252+seullll@users.noreply.github.com>
2023-11-09 20:21:04 +0900

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 const app = express();
 
-const port = process.env.PORT || 8000; // 포트 번호를 환경 변수 또는 기본값으로 설정
+const port = process.env.PORT || 8000 || 5000 || 3000; // 포트 번호를 환경 변수 또는 기본값으로 설정
 
 app.get("/ping", (req, res) => {
     res.send("pong");
